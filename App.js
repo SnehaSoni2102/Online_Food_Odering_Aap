8d978466b7356bd700f7a7ccba7c9401cdcd00ed
sneha <snehasoni2102@gmail.com>
2023-06-03 02:23:07 +0530

    added card items

--- App.js
+++ App.js
@@ -31,6 +31,9 @@ const RestaurantCard = () =>{
              src="https://media.gettyimages.com/id/1212329362/photo/table-top-view-of-indian-food.jpg?s=612x612&w=gi&k=20&c=8ZvQ1cg8__pgyLxl6TPpLfAfbBhzR-OYoj4jwSCbQ84="
             />
              <h3>Meghana foods</h3>
+             <h4>Pizza, North Indian, Asian</h4>
+             <h5>4.4</h5>
+             <h6>38 minutes</h6>
           </div>
     );
 };
@@ -40,6 +43,13 @@ const Body = () =>{
             <div className="search">Search</div>
             <div className="res-container">
                 <RestaurantCard />
+                <RestaurantCard />
+                <RestaurantCard />
+                <RestaurantCard />
+                <RestaurantCard />
+                <RestaurantCard />
+               
+                
             </div>
         </div>
     );
